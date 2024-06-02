820bb561d7cc5afa7aa29dd8dff57ebdb9636b95
Rahmaaliaaa <160307528+Rahmaaliaaa@users.noreply.github.com>
2024-06-02 21:22:34 +0700

    Add files via upload

---
+++ list_remove.py
@@ -0,0 +1,13 @@
+#  remove() : menghapus item yang ditentukan
+#input = thislist = ["apple", "banana", "cherry"]
+#Output = hapus pisang
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+# pop() : menghapus indeks yang ditentukan
+# input : thislist = ["apple", "banana", "cherry"]
+# Output : Hapus item kedua
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(2)
+print(thislist)
